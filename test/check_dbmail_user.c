31fa2369dd1a29b54659ed24c26ba2e607d1fcdf
Alan Hicks <ahicks@p-o.co.uk>
2026-07-16 13:08:19 +0100

    Password length check changed to 130 for sha512 Note: postgres db is 130 and mysql 255

--- test/check_dbmail_user.c
+++ test/check_dbmail_user.c
@@ -74,7 +74,29 @@ START_TEST(test_do_add)
 	int result;
 	result = do_add("testfailuser","testpass","md5-hash",0,0,NULL,NULL);
 	fail_unless(result!=0,"do_add succeeded when it should have failed");
-	result = do_add("testadduser","testpass","md5-hash",0,0,NULL,NULL);
+	result = do_add("testadduser-md5-hash","testpass","md5-hash",0,0,NULL,NULL);
+	fail_unless(result==0,"do_add failed");
+	result = do_add("testadduser-md5-digest","testpass","md5-digest",0,0,NULL,NULL);
+	fail_unless(result==0,"do_add failed");
+	result = do_add("testadduser-crypt-raw","testpass","crypt-raw",0,0,NULL,NULL);
+	fail_unless(result==0,"do_add failed");
+	result = do_add("testadduser-md5-hash-raw","testpass","md5-hash-raw",0,0,NULL,NULL);
+	fail_unless(result==0,"do_add failed");
+	result = do_add("testadduser-md5-digest-raw","testpass","md5-digest-raw",0,0,NULL,NULL);
+	fail_unless(result==0,"do_add failed");
+	result = do_add("testadduser-md5-base64","testpass","md5-base64",0,0,NULL,NULL);
+	fail_unless(result==0,"do_add failed");
+	result = do_add("testadduser-md5-base64-raw","testpass","md5-base64-raw",0,0,NULL,NULL);
+	fail_unless(result==0,"do_add failed");
+	result = do_add("testadduser-whirlpool","testpass","whirlpool",0,0,NULL,NULL);
+	fail_unless(result==0,"do_add failed");
+	result = do_add("testadduser-sha512","testpass","sha512",0,0,NULL,NULL);
+	fail_unless(result==0,"do_add failed");
+	result = do_add("testadduser-sha256","testpass","sha256",0,0,NULL,NULL);
+	fail_unless(result==0,"do_add failed");
+	result = do_add("testadduser-sha1","testpass","sha1",0,0,NULL,NULL);
+	fail_unless(result==0,"do_add failed");
+	result = do_add("testadduser-tiger","testpass","tiger",0,0,NULL,NULL);
 	fail_unless(result==0,"do_add failed");
 }
 END_TEST
@@ -102,10 +124,65 @@ START_TEST(test_do_delete)
 {
 	int result;
 	uint64_t user_idnr;
-	auth_user_exists("testadduser",&user_idnr);
-	fail_unless(user_idnr > 0,"abort test_do_delete: can't find user_idnr");
-	result = do_delete(user_idnr, "testadduser");
-	fail_unless(result==0,"test_do_delete failed");
+	auth_user_exists("testadduser-md5-hash", &user_idnr);
+	ck_assert_int_gt(user_idnr, 0);
+	result = do_delete(user_idnr, "testadduser-md5-hash");
+	ck_assert_int_eq(result, 0);
+
+	auth_user_exists("testadduser-md5-digest", &user_idnr);
+	ck_assert_int_gt(user_idnr, 0);
+	result = do_delete(user_idnr, "testadduser-md5-digest");
+	ck_assert_int_eq(result, 0);
+
+	auth_user_exists("testadduser-crypt-raw", &user_idnr);
+	ck_assert_int_gt(user_idnr, 0);
+	result = do_delete(user_idnr, "testadduser-crypt-raw");
+	ck_assert_int_eq(result, 0);
+
+	auth_user_exists("testadduser-md5-hash-raw", &user_idnr);
+	ck_assert_int_gt(user_idnr, 0);
+	result = do_delete(user_idnr, "testadduser-md5-hash-raw");
+	ck_assert_int_eq(result, 0);
+
+	auth_user_exists("testadduser-md5-digest-raw", &user_idnr);
+	ck_assert_int_gt(user_idnr, 0);
+	result = do_delete(user_idnr, "testadduser-md5-digest-raw");
+	ck_assert_int_eq(result, 0);
+
+	auth_user_exists("testadduser-md5-base64", &user_idnr);
+	ck_assert_int_gt(user_idnr, 0);
+	result = do_delete(user_idnr, "testadduser-md5-base64");
+	ck_assert_int_eq(result, 0);
+
+	auth_user_exists("testadduser-md5-base64-raw", &user_idnr);
+	ck_assert_int_gt(user_idnr, 0);
+	result = do_delete(user_idnr, "testadduser-md5-base64-raw");
+	ck_assert_int_eq(result, 0);
+
+	auth_user_exists("testadduser-whirlpool", &user_idnr);
+	ck_assert_int_gt(user_idnr, 0);
+	result = do_delete(user_idnr, "testadduser-whirlpool");
+	ck_assert_int_eq(result, 0);
+
+	auth_user_exists("testadduser-sha512", &user_idnr);
+	ck_assert_int_gt(user_idnr, 0);
+	result = do_delete(user_idnr, "testadduser-sha512");
+	ck_assert_int_eq(result, 0);
+
+	auth_user_exists("testadduser-sha256", &user_idnr);
+	ck_assert_int_gt(user_idnr, 0);
+	result = do_delete(user_idnr, "testadduser-sha256");
+	ck_assert_int_eq(result, 0);
+
+	auth_user_exists("testadduser-sha1", &user_idnr);
+	ck_assert_int_gt(user_idnr, 0);
+	result = do_delete(user_idnr, "testadduser-sha1");
+	ck_assert_int_eq(result, 0);
+
+	auth_user_exists("testadduser-tiger", &user_idnr);
+	ck_assert_int_gt(user_idnr, 0);
+	result = do_delete(user_idnr, "testadduser-tiger");
+	ck_assert_int_eq(result, 0);
 }
 END_TEST
 
